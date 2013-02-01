9744319794e98701c961bde250b044cd55616212
johanna-zumer <johanna.zumer@gmail.com>
2013-02-01 12:00:31 +0100

    enhancement - powerup and fsample now options

--- ni2_activation.m
+++ ni2_activation.m
@@ -17,6 +17,8 @@ function [data, time] = ni2_activation(varargin)
 %   length,    scalar value (in s): length of the activation time course,
 %                the sampling interval is fixed to 1 ms/sampl, default = 1
 %   ncycle,    scalar value: number of oscillatory cycles in the burst, default = 5
+%   powerup,    binary value: 1 = power burst (default); 0 = power loss
+%   fsample,   scalar value: sampling rate (Hz) (default 1000 Hz)
 %
 % Output arguments:
 %   data = 1xN vector with the activation time course
@@ -27,14 +29,13 @@ phase     = ft_getopt(varargin, 'phase',     0);
 latency   = ft_getopt(varargin, 'latency',   0.5);
 length    = ft_getopt(varargin, 'length',    1);
 ncycle    = ft_getopt(varargin, 'ncycle',    5);
+powerup   = ft_getopt(varargin, 'powerup',   1);
+fsample   = ft_getopt(varargin, 'fsample',   1000);
 
 maxncycle = 10*frequency*length;
 if ncycle > maxncycle
   error('the maximum number of allowable cycles is exceeded');
 end
-
-% fix the sample frequency at 1000 Hz
-fsample   = 1000;
 
 % create time axis that is 10 times as long as the requested length
 time      = (-round(5*length*fsample):round(5*length*fsample))./fsample;
@@ -49,6 +50,9 @@ nkrn       = numel(hannkrn);
 
 % create a taper that is centered at time 0
 taper      = [zeros(1,floor((10*n+1-nkrn)/2)) hannkrn(:)' zeros(1,ceil((10*n+1-nkrn)/2))];
+if ~powerup
+  taper = 1-taper;
+end
 
 % create the data
 data       = cos(phs).*taper;

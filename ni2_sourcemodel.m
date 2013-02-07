a16b26bd8130c4d12e1f55eb6171e2fa3b55038b
Jan-Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2013-02-07 14:53:08 +0100

    added new function

---
+++ ni2_sourcemodel.m
@@ -0,0 +1,24 @@
+function sourcemodel = ni2_sourcemodel(varargin)
+
+type = ft_getopt(varargin, 'type', 'grid');
+switch type
+  case 'grid'
+    resolution = ft_getopt(varargin, 'resolution', 0.5);
+    ax         = 0:resolution:9;
+    ax         = [-fliplr(ax(2:end)) ax];
+    [x,y,z]    = ndgrid(ax, ax, ax(ax>-1));
+    in         = false(size(x));
+    in(sqrt(x.^2+y.^2+z.^2)<=9) = true;
+    
+    pos        = [x(:) y(:) z(:)];
+    inside     = find(in);
+    outside    = find(in==0);
+    clear x y z
+    
+    sourcemodel.pos     = pos;
+    sourcemodel.inside  = inside;
+    sourcemodel.outside = outside;
+  case 'mesh'
+  otherwise
+    error('unsupported type of sourcemodel requested');
+end

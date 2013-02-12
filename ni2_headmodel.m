8881a05ace87972592d106c87596417d384387d0
Jan-Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2013-02-12 14:47:12 +0100

    changed api to key-value pairs in input

--- ni2_headmodel.m
+++ ni2_headmodel.m
@@ -1,23 +1,26 @@
-function headmodel = ni2_headmodel(n)
+function headmodel = ni2_headmodel(varargin)
 
-if nargin<1
-  n = 1;
-end
+type = ft_getopt(varargin, 'type',    'spherical');
+n    = ft_getopt(varargin, 'nshells', 1);
 
-switch n
-  case 1
-    headmodel.o    = [0 0 0];
-    headmodel.r    = 10;
-    headmodel.c    = 1;
-    headmodel.unit = 'cm';
-    headmodel      = ft_datatype_headmodel(headmodel);
-  case 3
-    headmodel.o    = [0 0 0];
-    headmodel.r    = [0.88 0.92 1.00]*10;
-    headmodel.c    = [1 1/80 1];
-    headmodel.unit = 'cm';
-    headmodel      = ft_datatype_headmodel(headmodel);
+switch type
+  case 'spherical'
+    switch n
+      case 1
+        headmodel.o    = [0 0 0];
+        headmodel.r    = 10;
+        headmodel.c    = 1;
+        headmodel.unit = 'cm';
+        headmodel      = ft_datatype_headmodel(headmodel);
+      case 3
+        headmodel.o    = [0 0 0];
+        headmodel.r    = [0.88 0.92 1.00]*10;
+        headmodel.c    = [1 1/80 1];
+        headmodel.unit = 'cm';
+        headmodel      = ft_datatype_headmodel(headmodel);
+      otherwise
+        error('number of spheres other than 1 or 3 is not supported');
+    end
   otherwise
-    error('number of spheres other than 1 or 3 is not supported');
+    error('unknown type requested');
 end
-

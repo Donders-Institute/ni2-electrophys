0850ac30d331697452130581cfd5141eedf08ec8
Robert Oostenveld <r.oostenveld@gmail.com>
2022-03-21 09:36:42 +0100

    draw figure immediately

--- ni2_montageplot.m
+++ ni2_montageplot.m
@@ -9,10 +9,13 @@ datnew = zeros(siz(2)*n1, siz(1)*n2)+nan;
 for k = 1:n
   ix2 = rem(k-1, n2) + 1;
   ix1 = floor((k-1)/n2) + 1;
-  
+
   indx2 = (ix2-1)*siz(2) + (1:siz(2));
   indx1 = (ix1-1)*siz(1) + (1:siz(1));
- 
+
   datnew(indx1, indx2) = dat(:,:,k);
 end
 figure; imagesc(datnew);axis xy
+
+% update the figure immediately
+drawnow

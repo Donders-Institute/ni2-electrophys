e7fc02fec9efc6d56e64a1b631f2203884d35daa
Robert Oostenveld <r.oostenveld@gmail.com>
2024-02-29 11:04:20 +0100

    improve robustness of path setup

--- ni2_startup.m
+++ ni2_startup.m
@@ -10,15 +10,26 @@ function ni2_startup
 fprintf('clearing all variables from the workspace\n');
 evalin('base', 'clear')
 
-fprintf('restoring the default MATLAB-path\n');
-restoredefaultpath;
-
 ni2_dir = fileparts(mfilename('fullpath')); % this file itself is contained in the ni2 directory
-ft_dir  = fullfile(fileparts(ni2_dir), 'fieldtrip');
+ft_dir  = fileparts(which('ft_defaults'));
+
+if ~exist(ft_dir, 'dir')
+  % see whether we can find it next to the ni2 directory
+  d = dir(fileparts(ni2_dir));
+  select = find(startsWith({d.name}, 'fieldtrip'), 1);
+  ft_dir = fullfile(fileparts(ni2_dir), d(select).name);
+  if ~exist(fullfile(ft_dir, 'ft_defaults.m'), 'file')
+    % no luck
+    ft_dir = [];
+  end
+end
 
 if ~exist(ft_dir, 'dir')
   warning('cannot locate fieldtrip directory')
 end
+
+fprintf('restoring the default MATLAB-path\n');
+restoredefaultpath;
 
 % add the ni2 and fieldtrip directories to the path
 fprintf('adding % s to the MATLAB-path\n', ni2_dir);
@@ -27,9 +38,9 @@ addpath(ni2_dir);
 fprintf('adding % s to the MATLAB-path\n', ft_dir);
 addpath(ft_dir);
 
-% add the necessary subdirectories of FieldTrip to the path
 try
+  % add the necessary subdirectories of FieldTrip to the path
   ft_defaults;
 catch
-  warning('cannot locate ft_defaults')
+  warning('cannot add the necessary subdirectories of FieldTrip to the path')
 end
